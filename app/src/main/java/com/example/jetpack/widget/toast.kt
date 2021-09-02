cb262bd6cc1a665504af70c5c98b73bd191a7979
deepajais95 <shweta@witbybit.com>
2021-09-02 21:05:14 +0530

    Create toast.kt

---
+++ app/src/main/java/com/example/jetpack/widget/toast.kt
@@ -0,0 +1,30 @@
+package com.example.jetpack.widget
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+
+@Composable
+fun ToastDemo() {
+    val context = LocalContext.current
+    Column(
+        content = {
+            Button(onClick = {
+                Toast.makeText(
+                    context,
+                    "Showing toast....",
+                    Toast.LENGTH_LONG
+                ).show()
+            }, content = {
+                Text(text = "Show Toast")
+            })
+        }, modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+}

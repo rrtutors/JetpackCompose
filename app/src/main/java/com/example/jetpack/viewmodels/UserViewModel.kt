09e7f06989808e8fc8931a57c6c96176d1ece8e2
deepajais95 <shweta@witbybit.com>
2021-09-27 23:53:41 +0530

    added expanded list

---
+++ app/src/main/java/com/example/jetpack/viewmodels/UserViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.jetpack.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.jetpack.datamodels.User
+import com.example.jetpack.widget.ExpandedListItem
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class UserViewModel(appObj: Application) : AndroidViewModel(appObj) {
+    private val _users = MutableStateFlow(listOf<ExpandedListItem>())
+    val users: StateFlow<List<ExpandedListItem>> get() = _users
+
+    fun changeItemValue(index: Int, expanded: Boolean) {
+        val list = _users.value.toMutableList()
+        list[index] = list[index].copy(expanded = expanded)
+        _users.value = list.toList()
+    }
+    init {
+        fetchUsers()
+    }
+
+    private fun fetchUsers() {
+        viewModelScope.launch {
+            val userList = arrayListOf(
+                ExpandedListItem(
+                    data = User(id = "1", name = "Ankit Singh", emailId = "abc@gmail.com"),
+                    index = 0
+                ),
+                ExpandedListItem(
+                    data = User(id = "2", name = "Neha Shaw", emailId = "xyz@gmail.com"),
+                    index = 1
+                ),
+                ExpandedListItem(
+                    data = User(id = "3", name = "Arpita Ghosh", emailId = "abc@gmail.com"),
+                    index = 2
+                ),
+                ExpandedListItem(
+                    data = User(id = "4", name = "Akash Tiwari", emailId = "xyz@gmail.com"),
+                    index = 3
+                )
+            )
+            _users.emit(userList)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/jetpack/widget/expandedList.kt
@@ -0,0 +1,78 @@
+package com.example.jetpack.widget
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ArrowDropUp
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.jetpack.datamodels.User
+import com.example.jetpack.ui.theme.JetPackTheme
+import com.example.jetpack.viewmodels.UserViewModel
+
+data class ExpandedListItem(val data: User, val index: Int, var expanded: Boolean = false)
+
+@Composable
+fun ExpandableListDemo(userViewModel: UserViewModel) {
+    val users = userViewModel.users.collectAsState()
+
+    JetPackTheme() {
+        LazyColumn(contentPadding = PaddingValues(horizontal = 16.dp)) {
+            itemsIndexed(items = users.value, itemContent = { pos, item ->
+                Column(content = {
+                    ExpandedListItemView(
+                        item,
+                        onButtonClicked = { id: String, expanded: Boolean, index: Int ->
+                            userViewModel.changeItemValue(
+                                index,
+                                expanded = expanded
+                            )
+                        })
+                    if (item.expanded) {
+                        UserListItemView(user = item.data)
+                    }
+                })
+            })
+
+        }
+    }
+}
+
+@Composable
+fun ExpandedListItemView(
+    item: ExpandedListItem,
+    onButtonClicked: ((id: String, expanded: Boolean, index: Int) -> Unit)? = null
+) {
+    Row(
+        content = {
+            Text(item.data.name, modifier = Modifier.weight(1F), fontWeight = FontWeight.Bold)
+            Icon(
+                imageVector = if (item.expanded) Icons.Default.ArrowDropUp else Icons.Default.ArrowDropDown,
+                contentDescription = "image",
+                tint = Color.Black, modifier = Modifier
+                    .size(30.dp)
+                    .clickable(onClick = {
+                        onButtonClicked?.invoke(item.data.id, !item.expanded, item.index)
+                    })
+            )
+        }, modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp)
+    )
+}
+
+@Composable
+fun UserListItemView(user: User) {
+    Column(content = {
+        Text(text = user.emailId, color = Color.Gray)
+    })
+}

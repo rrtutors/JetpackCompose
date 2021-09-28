ca6b4704022d2af68355612d617a63d05efcb3b3
deepajais95 <shweta@witbybit.com>
2021-09-28 20:11:14 +0530

    Update UserViewModel.kt

--- app/src/main/java/com/example/jetpack/viewmodels/UserViewModel.kt
+++ app/src/main/java/com/example/jetpack/viewmodels/UserViewModel.kt
@@ -40,6 +40,30 @@ class UserViewModel(appObj: Application) : AndroidViewModel(appObj) {
                 ExpandedListItem(
                     data = User(id = "4", name = "Akash Tiwari", emailId = "xyz@gmail.com"),
                     index = 3
+                ),
+                ExpandedListItem(
+                    data = User(id = "5", name = "Anisha Tiwari", emailId = "abc@gmail.com"),
+                    index = 4
+                ),
+                ExpandedListItem(
+                    data = User(id = "6", name = "Rowdy Rathore", emailId = "xyz@gmail.com"),
+                    index = 5
+                ),
+                ExpandedListItem(
+                    data = User(id = "7", name = "Jit Singh", emailId = "abc@gmail.com"),
+                    index = 6
+                ),
+                ExpandedListItem(
+                    data = User(id = "8", name = "Pravin Raj", emailId = "xyz@gmail.com"),
+                    index = 7
+                ),
+                ExpandedListItem(
+                    data = User(id = "9", name = "Sneha Rao", emailId = "abc@gmail.com"),
+                    index = 8
+                ),
+                ExpandedListItem(
+                    data = User(id = "10", name = "Ranjana Rathore", emailId = "xyz@gmail.com"),
+                    index = 9
                 )
             )
             _users.emit(userList)
